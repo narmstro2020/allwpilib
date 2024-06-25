8941d2259daaacd278861e049a3b45cc95bb3ef6
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-06-25 10:57:48 -0400

    java version rough draft

--- wpimath/src/main/java/edu/wpi/first/math/controller/ElevatorFeedforward.java
+++ wpimath/src/main/java/edu/wpi/first/math/controller/ElevatorFeedforward.java
@@ -4,11 +4,14 @@
 
 package edu.wpi.first.math.controller;
 
-import edu.wpi.first.math.MatBuilder;
-import edu.wpi.first.math.Nat;
+import static edu.wpi.first.units.Units.Volts;
+
 import edu.wpi.first.math.controller.proto.ElevatorFeedforwardProto;
 import edu.wpi.first.math.controller.struct.ElevatorFeedforwardStruct;
-import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.units.Measure;
+import edu.wpi.first.units.Unit;
+import edu.wpi.first.units.Velocity;
+import edu.wpi.first.units.Voltage;
 import edu.wpi.first.util.protobuf.ProtobufSerializable;
 import edu.wpi.first.util.struct.StructSerializable;
 
@@ -29,6 +32,9 @@ public class ElevatorFeedforward implements ProtobufSerializable, StructSerializ
   /** The acceleration gain. */
   public final double ka;
 
+  /** The period. */
+  private double m_dt;
+
   /** ElevatorFeedforward protobuf for serialization. */
   public static final ElevatorFeedforwardProto proto = new ElevatorFeedforwardProto();
 
@@ -43,10 +49,12 @@ public class ElevatorFeedforward implements ProtobufSerializable, StructSerializ
    * @param kg The gravity gain.
    * @param kv The velocity gain.
    * @param ka The acceleration gain.
+   * @param dtSeconds The period in seconds.
    * @throws IllegalArgumentException for kv &lt; zero.
    * @throws IllegalArgumentException for ka &lt; zero.
+   * @throws IllegalArgumentException for period &le; zero.
    */
-  public ElevatorFeedforward(double ks, double kg, double kv, double ka) {
+  public ElevatorFeedforward(double ks, double kg, double kv, double ka, double dtSeconds) {
     this.ks = ks;
     this.kg = kg;
     this.kv = kv;
@@ -57,15 +65,37 @@ public class ElevatorFeedforward implements ProtobufSerializable, StructSerializ
     if (ka < 0.0) {
       throw new IllegalArgumentException("ka must be a non-negative number, got " + ka + "!");
     }
+    if (dtSeconds <= 0.0) {
+      throw new IllegalArgumentException(
+          "period must be a positive number, got " + dtSeconds + "!");
+    }
+    m_dt = dtSeconds;
   }
 
   /**
-   * Creates a new ElevatorFeedforward with the specified gains. Acceleration gain is defaulted to
-   * zero. Units of the gain values will dictate units of the computed feedforward.
+   * Creates a new ElevatorFeedforward with the specified gains. The period is defaulted to 20 ms.
+   * Units of the gain values will dictate units of the computed feedforward.
    *
    * @param ks The static gain.
    * @param kg The gravity gain.
    * @param kv The velocity gain.
+   * @param ka The acceleration gain.
+   * @throws IllegalArgumentException for kv &lt; zero.
+   * @throws IllegalArgumentException for ka &lt; zero.
+   */
+  public ElevatorFeedforward(double ks, double kg, double kv, double ka) {
+    this(ks, kg, kv, ka, 0.020);
+  }
+
+  /**
+   * Creates a new ElevatorFeedforward with the specified gains. Acceleration gain is defaulted to
+   * zero. The period is defaulted to 20 ms. Units of the gain values will dictate units of the
+   * computed feedforward.
+   *
+   * @param ks The static gain.
+   * @param kg The gravity gain.
+   * @param kv The velocity gain.
+   * @throws IllegalArgumentException for kv &lt; zero.
    */
   public ElevatorFeedforward(double ks, double kg, double kv) {
     this(ks, kg, kv, 0);
@@ -78,50 +108,10 @@ public class ElevatorFeedforward implements ProtobufSerializable, StructSerializ
    * @param acceleration The acceleration setpoint.
    * @return The computed feedforward.
    */
+  @SuppressWarnings("removal")
+  @Deprecated(forRemoval = true, since = "2025")
   public double calculate(double velocity, double acceleration) {
     return ks * Math.signum(velocity) + kg + kv * velocity + ka * acceleration;
-  }
-
-  /**
-   * Calculates the feedforward from the gains and setpoints.
-   *
-   * <p>Note this method is inaccurate when the velocity crosses 0.
-   *
-   * @param currentVelocity The current velocity setpoint.
-   * @param nextVelocity The next velocity setpoint.
-   * @param dtSeconds Time between velocity setpoints in seconds.
-   * @return The computed feedforward.
-   */
-  public double calculate(double currentVelocity, double nextVelocity, double dtSeconds) {
-    // Discretize the affine model.
-    //
-    //   dx/dt = Ax + Bu + c
-    //   dx/dt = Ax + B(u + B⁺c)
-    //   xₖ₊₁ = eᴬᵀxₖ + A⁻¹(eᴬᵀ - I)B(uₖ + B⁺cₖ)
-    //   xₖ₊₁ = A_d xₖ + B_d (uₖ + B⁺cₖ)
-    //   xₖ₊₁ = A_d xₖ + B_duₖ + B_d B⁺cₖ
-    //
-    // Solve for uₖ.
-    //
-    //   B_duₖ = xₖ₊₁ − A_d xₖ − B_d B⁺cₖ
-    //   uₖ = B_d⁺(xₖ₊₁ − A_d xₖ − B_d B⁺cₖ)
-    //   uₖ = B_d⁺(xₖ₊₁ − A_d xₖ) − B⁺cₖ
-    //
-    // For an elevator with the model
-    // dx/dt = -Kv/Ka x + 1/Ka u - Kg/Ka - Ks/Ka sgn(x),
-    // A = -Kv/Ka, B = 1/Ka, and c = -(Kg/Ka + Ks/Ka sgn(x)). Substitute in B
-    // assuming sgn(x) is a constant for the duration of the step.
-    //
-    //   uₖ = B_d⁺(xₖ₊₁ − A_d xₖ) − Ka(-(Kg/Ka + Ks/Ka sgn(x)))
-    //   uₖ = B_d⁺(xₖ₊₁ − A_d xₖ) + Ka(Kg/Ka + Ks/Ka sgn(x))
-    //   uₖ = B_d⁺(xₖ₊₁ − A_d xₖ) + Kg + Ks sgn(x)
-    var plant = LinearSystemId.identifyVelocitySystem(this.kv, this.ka);
-    var feedforward = new LinearPlantInversionFeedforward<>(plant, dtSeconds);
-
-    var r = MatBuilder.fill(Nat.N1(), Nat.N1(), currentVelocity);
-    var nextR = MatBuilder.fill(Nat.N1(), Nat.N1(), nextVelocity);
-
-    return kg + ks * Math.signum(currentVelocity) + feedforward.calculate(r, nextR).get(0, 0);
   }
 
   /**
@@ -131,8 +121,100 @@ public class ElevatorFeedforward implements ProtobufSerializable, StructSerializ
    * @param velocity The velocity setpoint.
    * @return The computed feedforward.
    */
+  @SuppressWarnings("removal")
+  @Deprecated(forRemoval = true, since = "2025")
   public double calculate(double velocity) {
     return calculate(velocity, 0);
+  }
+
+  /**
+   * Calculates the feedforward from the gains and setpoints assuming discrete control when the
+   * setpoint does not change.
+   *
+   * @param <U> The velocity parameter either as distance or angle.
+   * @param setpoint The velocity setpoint.
+   * @return The computed feedforward.
+   */
+  public <U extends Unit<U>> Measure<Voltage> calculate(Measure<Velocity<U>> setpoint) {
+    return calculate(setpoint, setpoint);
+  }
+
+  /**
+   * Calculates the feedforward from the gains and setpoints.
+   *
+   * <p>Note this method is inaccurate when the velocity crosses 0.
+   *
+   * @param <U> The velocity parameter either as distance or angle.
+   * @param currentVelocity The current velocity setpoint.
+   * @param nextVelocity The next velocity setpoint.
+   * @return The computed feedforward.
+   */
+  public <U extends Unit<U>> Measure<Voltage> calculate(
+      Measure<Velocity<U>> currentVelocity, Measure<Velocity<U>> nextVelocity) {
+    if (ka == 0.0) {
+      // Given the following discrete feedforward model
+      //
+      //   uₖ = B_d⁺(rₖ₊₁ − A_d rₖ)
+      //
+      // where
+      //
+      //   A_d = eᴬᵀ
+      //   B_d = A⁻¹(eᴬᵀ - I)B
+      //   A = −kᵥ/kₐ
+      //   B = 1/kₐ
+      //
+      // We want the feedforward model when kₐ = 0.
+      //
+      // Simplify A.
+      //
+      //   A = −kᵥ/kₐ
+      //
+      // As kₐ approaches zero, A approaches -∞.
+      //
+      //   A = −∞
+      //
+      // Simplify A_d.
+      //
+      //   A_d = eᴬᵀ
+      //   A_d = exp(−∞)
+      //   A_d = 0
+      //
+      // Simplify B_d.
+      //
+      //   B_d = A⁻¹(eᴬᵀ - I)B
+      //   B_d = A⁻¹((0) - I)B
+      //   B_d = A⁻¹(-I)B
+      //   B_d = -A⁻¹B
+      //   B_d = -(−kᵥ/kₐ)⁻¹(1/kₐ)
+      //   B_d = (kᵥ/kₐ)⁻¹(1/kₐ)
+      //   B_d = kₐ/kᵥ(1/kₐ)
+      //   B_d = 1/kᵥ
+      //
+      // Substitute these into the feedforward equation.
+      //
+      //   uₖ = B_d⁺(rₖ₊₁ − A_d rₖ)
+      //   uₖ = (1/kᵥ)⁺(rₖ₊₁ − (0) rₖ)
+      //   uₖ = kᵥrₖ₊₁
+      return Volts.of(
+          kg + ks * Math.signum(nextVelocity.magnitude()) + kv * nextVelocity.magnitude());
+    } else {
+      //   uₖ = B_d⁺(rₖ₊₁ − A_d rₖ)
+      //
+      // where
+      //
+      //   A_d = eᴬᵀ
+      //   B_d = A⁻¹(eᴬᵀ - I)B
+      //   A = −kᵥ/kₐ
+      //   B = 1/kₐ
+      double A = -kv / ka;
+      double B = 1.0 / ka;
+      double A_d = Math.exp(A * m_dt);
+      double B_d = 1.0 / A * (A_d - 1.0) * B;
+      return Volts.of(
+          kg
+              + ks * Math.signum(currentVelocity.magnitude())
+              + 1.0 / B_d * (nextVelocity.magnitude() - A_d * currentVelocity.magnitude()));
+    }
   }
 
   // Rearranging the main equation from the calculate() method yields the
